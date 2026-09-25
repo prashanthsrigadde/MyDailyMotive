e7f9bb7c29d80bf9373f3247581c57d3093a0d46
prashanthsrigadde <prashanth.srigadde@gmail.com>
2026-09-25 22:32:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE='my-daily-motive-v3';
+const CACHE='my-daily-motive-v3-2';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./sw.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{
+ if(e.request.mode==='navigate'){
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
+ }else{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(y=>y.put(e.request,c));return x})));
+ }
+});
